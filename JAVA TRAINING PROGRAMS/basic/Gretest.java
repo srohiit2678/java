ce103a9ec0942bb0f595288859277a78e26f64e5
Rohit Sikhwal <rohitsikhwal99@gmail.com>
2024-09-11 01:24:47 +0530

    Create Gretest.java

---
+++ JAVA TRAINING PROGRAMS/basic/Gretest.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Swapping{
+public static void main(String []args){
+Scanner sc = new Scanner(System.in);
+char ch;
+do{
+System.out.print("Enter the 1st Number : ");
+int x = sc.nextInt();
+System.out.print("Enter the 2nd Number : ");
+int y = sc.nextInt();
+System.out.print("Enter the 3rd Number : ");
+int z = sc.nextInt();
+
+	if(x > y && x > z){
+		System.out.println("Gretest Number is :" + x);
+	}
+	else if(y > x && y > z){
+		System.out.println("Gretest Number is :" + y);
+	}
+	else {
+		System.out.println("Gretest Number is :" + z);
+	}
+	System.out.println("--------- Do You Want To Exit :(YES or NO)-----------");
+	  ch = sc.next().charAt(0);
+}while(ch=='N' ||  ch=='n');
+}
+}
