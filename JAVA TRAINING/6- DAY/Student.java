630a9849543adbb17fab2a7a37aa2ad4f5d5dcc0
Rohit Sikhwal <rohitsikhwal99@gmail.com>
2024-09-11 01:24:15 +0530

    delete unwantet files

--- JAVA TRAINING/6- DAY/Student.java
+++
@@ -1,7 +0,0 @@
-public class Student {
-    public static void main(){
-        // fully collyfied class name
-        java.util.Date dt = new java.util.Date();
-        System.out.println("TODAY IS: "+dt);
-    }    
-}
