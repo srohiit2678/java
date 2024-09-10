b2e2d9eb920306e7c011e3bcdd3da91c2009f344
Rohit Sikhwal <rohitsikhwal99@gmail.com>
2024-09-11 01:27:26 +0530

    Create Table.java

---
+++ JAVA TRAINING PROGRAMS/basic/Table.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Table{
+public static void main(String []args){
+Scanner sc = new Scanner(System.in);
+char ch;
+do{
+System.out.print("Enter the Number for Table : ");
+int x = sc.nextInt();
+	for(int i=1;i<=10;i++){
+	
+		System.out.println(x+"  X "+i+" = " + (i*x));
+
+	}
+		System.out.println("--------- Do You Want To Exit :(YES or NO)-----------");
+	  ch = sc.next().charAt(0);
+}while(ch=='N' ||  ch=='n');
+	
+}
+}
