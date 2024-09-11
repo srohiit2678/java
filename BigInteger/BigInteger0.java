ea7c83c4addc5bb41e8aa10abf8bdcb7812e2610
Rohit Sikhwal <rohitsikhwal99@gmail.com>
2024-09-11 17:07:46 +0530

    learn about BigInteger in java

---
+++ BigInteger/BigInteger0.java
@@ -0,0 +1,19 @@
+
+// all java class have a method -> valueOf();
+
+public class BigInteger0{
+public static void main(String[] args){
+
+int x = 10;
+String s = String.valueOf(x); // ""+x
+		// int to String
+System.out.println(s);
+System.out.println(x);
+
+ }
+}
+
+/*
+
+
+*/
---
+++ BigInteger/BigInteger1.java
@@ -0,0 +1,19 @@
+import java.math.*; // BigInteger pacakage
+
+// all java classes have a static method -> valueOf() 
+
+public class BigInteger1{
+public static void main(String[] args){
+
+int x = 10;
+BigInteger s = BigInteger.valueOf(x); 
+		// int to BigInteger
+System.out.println(s);
+System.out.println(x);
+
+
+ }
+}
+/*
+
+*/
