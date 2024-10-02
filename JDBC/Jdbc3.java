f783fcf81ea2dfca74092981ed82328b2a62d57a
srohiit2678 <rohitsikhwal99@gmail.com>
2024-10-03 01:10:07 +0530

    Create Jdbc3.java

---
+++ JDBC/Jdbc3.java
@@ -0,0 +1,81 @@
+/*
+-------- JDBC -------------
+-> import package
+-> Load and Register the Driver
+-> Opening database connection to MySql server
+-> getting Statement object to execute query
+-> execute query
+-> close connection
+===========================================================
+	1.import package 
+		-> import java.sql.*;
+	
+===========================================================
+	2.Load and Register the Driver
+		-> Class.forName("com.mysql.cj.jdbc.Driver");
+	
+===========================================================
+	3.Opening database connection to MySql server
+		->  String db_url ="jdbc:mysql://localhost:3306/RDB?useSSL=false";
+		->  String db_uname ="root";
+		->  String db_upass ="root";
+		->  Connection con= DriverManager.getConnection(db_url,db_uname,db_upass);
+
+===========================================================
+	4.getting Statement object to execute query
+		->	Statement st = con.createStatement();
+
+===========================================================	
+    5.execute query
+	    -> String q = "insert into login(uname,upass) values('swt',258)";
+		-> st.executeUpate(q);
+
+===========================================================
+	6.close connection
+		-> con.close();
+===========================================================
+ --> for temprary class path set 
+set classpath=C:\Users\Rohit\OneDrive\Desktop\java\JDBC\mysql-connector-j-8.1.0.jar;.;%classpath%
+*/
+
+// 1.import package
+import java.sql.*;
+import java.util.Scanner;
+
+ class Jdbc3 {
+ public static void main(String []args){
+     Scanner sc = new Scanner(System.in);
+	
+	System.out.println("Enter Name: ");
+    String s1 = sc.next();
+ 
+    System.out.println("Enter Pass: ");
+	String s2 = sc.next();
+  try{
+	  //	2.Load and Register the Driver
+	     Class.forName("com.mysql.cj.jdbc.Driver");
+		              //"com.mysql.jdbc.Driver"    
+	  
+	  //    3.Opening database connection to MySql server
+	     String db_url ="jdbc:mysql://localhost:3306/RDB?useSSL=false&allowPublicKeyRetrieval=true";
+		 String db_uname ="root";
+		 String db_upass ="root";
+		 Connection con = DriverManager.getConnection(db_url,db_uname,db_upass);
+     
+	  //    4.getting Statement object to execute query
+		 Statement st = con.createStatement();
+	  
+	  //    5.execute query
+	     String q = "insert into login(uname,upass) values('"+s1+"','"+s2+"')";
+		 // String q = "insert into login values('swt',258)";
+		 st.executeUpdate(q);	
+		System.out.println("data insert....");
+	  //    6.close connection
+		 con.close();
+	     sc.close();
+  }
+  catch(Exception e){
+	  System.out.println(e);
+  }
+ }
+}
