03e42d39fcb77c77dcac0d150dbc09dcf122a5fa
srohiit2678 <rohitsikhwal99@gmail.com>
2024-11-09 10:15:17 +0530

    Create DemoB.java

---
+++ JAVA PACKAGE/learn/com/companyrohit/start/DemoB.java
@@ -0,0 +1,10 @@
+import com.companyrohit.start.DemoA;
+public class DemoB{
+	public int ad(int a,int b){
+return a+b;
+}
+public static void main(String []arg){
+DemoB a = new DemoB();
+System.out.println("sum: "+a.ad(3,3));
+}
+}
