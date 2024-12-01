b8cca9319f667f95731956097dcf6af809f5594f
srohiit2678 <rohitsikhwal99@gmail.com>
2024-12-01 19:14:15 +0530

    Create ReverseNumber.java

---
+++ JAVA TRAINING PROGRAMS/basic/ReverseNumber.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class ReverseNumber{
+public static void main(String []args){
+Scanner sc = new Scanner(System.in);
+char ch;
+
+do{
+System.out.print("Enter the Number for Reverse : ");
+int x = sc.nextInt();
+int r1=1,r2=0;
+ while(x > 0){
+	 r1 = x%10;
+	 r2 = r2*10+r1;
+	 x  = x/10;
+ }
+System.out.println("the Number after Reverse : "+r2);
+
+	System.out.println("--------- Do You Want To Exit :(YES or NO)-----------");
+	  ch = sc.next().charAt(0);
+}while(ch=='N' ||  ch=='n');
+sc.close();
+}
+}
