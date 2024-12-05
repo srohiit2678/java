80ed778f6ea91162faf0357fc5a206ca8438371f
srohiit2678 <rohitsikhwal99@gmail.com>
2024-12-05 10:27:54 +0530

    Create ApS.java

---
+++ JAVA TRAINING PROGRAMS/series/ApS.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class ApS {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+char ch;
+do{
+    System.out.print("Enter Starting Term :");
+    int start = sc.nextInt();
+    System.out.print("Enter the Difference : ");
+    int diff = sc.nextInt();
+    System.out.print("Enter the length of series :");
+   int len = sc.nextInt();
+    
+   for(int i=1;i<=len;i++){
+        
+        System.out.print((start)+" " );
+        start += diff;
+   }
+   
+    System.out.println("--------- Do You Want To Exit :(YES or NO)-----------");
+    ch = sc.next().charAt(0);
+}while(ch=='N' ||  ch=='n');
+sc.close();	
+    }
+}
