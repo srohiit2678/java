7e97a4b74c627131c491a0eab12aa46407bae3e7
srohiit2678 <rohitsikhwal99@gmail.com>
2025-01-25 12:13:19 +0530

    Create Demo12.java

---
+++ JAVA SIR/Demo12.java
@@ -0,0 +1,23 @@
+class Demo12{
+	public static void main(String Ar[]){
+		String []a = {"rohit","addi","mohit","suraj","yash","harshi","niru","arjun","kuldeep","sandeep","prathem"};
+		String temp;
+		for(int i = 0;i<a.length;i++){
+			for(int j=0;j<a.length-i-1;j++){
+		{
+		//	System.out.println((""+(a[0].charAt(0))).compareToIgnoreCase(""+(a[1].charAt(0))));
+			}
+		if(a[j].charAt(0)> a[j+1].charAt(0)){
+					temp = a[j];
+					a[j] = a[j+1];
+					a[j+1] = temp;
+				}	
+		}
+	}
+		
+		for(int j=0;j<a.length;j++){
+		System.out.print("  "+a[j]);
+		}
+
+	}
+}
