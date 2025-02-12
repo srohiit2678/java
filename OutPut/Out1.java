cdd6258abb6fe14d7da6c6d548ab155be24b9467
srohiit2678 <rohitsikhwal99@gmail.com>
2025-02-12 20:57:20 +0530

    Create Out1.java

---
+++ OutPut/Out1.java
@@ -0,0 +1,11 @@
+public class Out1
+{
+	public static void main(String[] args)
+	{
+		for(int i=0;i<10;i=i++) // i = i++ ==>  put at palce of i is 0 --> i = become 1 --> assign value in i = 0
+		{
+			System.out.println(i);
+		}
+		// infinite time 0
+	}
+}
