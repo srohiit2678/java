f23b95eb2b2138a1b9143fd2adc6f6e363394f22
srohiit2678 <rohitsikhwal99@gmail.com>
2025-02-24 21:49:37 +0530

    Update LoginUser.java

--- ADVANCE JAVA/Servlet/crud-project/WEB-INF/classes/LoginUser.java
+++ ADVANCE JAVA/Servlet/crud-project/WEB-INF/classes/LoginUser.java
@@ -1,36 +1,32 @@
-// Source code is decompiled from a .class file using FernFlower decompiler.
 import java.io.*;
 import java.sql.*;
 import javax.servlet.*;
-import javax.servlet.http.*;;
+import javax.servlet.http.*;
 
 public class LoginUser extends HttpServlet {
-   public LoginUser() {
-   }
-
-   public void doGet(HttpServletRequest var1, HttpServletResponse var2) throws IOException, ServletException {
-      PrintWriter var3 = var2.getWriter();
-      String var4 = var1.getParameter("uname");
-      String var5 = var1.getParameter("upass");
-      var3.println("<html><body>");
+   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+      PrintWriter out = response.getWriter();
+      String uname = request.getParameter("uname");
+      String upass = request.getParameter("upass");
+      out.println("<html><body>");
 
       try {
          Class.forName("com.mysql.cj.jdbc.Driver");
-         Connection var6 = DriverManager.getConnection("jdbc:mysql:///advjava", "root", "root");
-         Statement var7 = var6.createStatement();
-         ResultSet var8 = var7.executeQuery("select * from registration where uname='" + var4 + "' and upass='" + var5 + "'");
-         if (var8.next()) {
-            var2.sendRedirect("MenuPage.html");
+         Connection con = DriverManager.getConnection("jdbc:mysql:///advjava?useSSL=false", "root", "root");
+         Statement st = con.createStatement();
+         ResultSet rs = st.executeQuery("select * from registration where uname='" + uname + "' and upass='" + upass + "'");
+         if (rs.next()) {
+            response.sendRedirect("MenuPage.html");
          } else {
-            var3.println("<center>Invalide User Name Or Password</center>");
+            out.println("<center>Invalide User Name Or Password</center>");
          }
-
-         var6.close();
-      } catch (Exception var9) {
-         var3.println(var9);
+          con.close();
+       } 
+      catch (Exception e) {
+         out.println(e);
       }
 
-      var3.println("</body></html>");
-      var3.close();
+      out.println("</body></html>");
+      out.close();
    }
 }
