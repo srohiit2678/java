d550210fb37f08a609cc566b66c6742f97373350
srohiit2678 <rohitsikhwal99@gmail.com>
2025-02-24 21:49:43 +0530

    Update RegisterNewUser.java

--- ADVANCE JAVA/Servlet/crud-project/WEB-INF/classes/RegisterNewUser.java
+++ ADVANCE JAVA/Servlet/crud-project/WEB-INF/classes/RegisterNewUser.java
@@ -1,30 +1,28 @@
-// Source code is decompiled from a .class file using FernFlower decompiler.
 import java.io.*;
 import java.sql.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
 
 public class RegisterNewUser extends HttpServlet {
-   public void doGet(HttpServletRequest var1, HttpServletResponse var2) throws IOException, ServletException {
-      PrintWriter var3 = var2.getWriter();
-      String var4 = var1.getParameter("uname");
-      String var5 = var1.getParameter("upass");
-      String var6 = var1.getParameter("ucontact");
-      var3.println("<html><body>");
-      var3.println("<html><body>");
+   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+      PrintWriter out = response.getWriter();
+      String uname = request.getParameter("uname");
+      String upass = request.getParameter("upass");
+      String ucontact = request.getParameter("ucontact");
+      out.println("<html><body>");
+      out.println("<html><body>");
 
       try {
          Class.forName("com.mysql.cj.jdbc.Driver");
-         Connection var7 = DriverManager.getConnection("jdbc:mysql:///advjava", "root", "root");
-         Statement var8 = var7.createStatement();
-         var8.executeUpdate("insert into registration values('" + var4 + "','" + var5 + "','" + var6 + "')");
-         var7.close();
-      } catch (Exception var9) {
-         var3.println(var9);
+         Connection con = DriverManager.getConnection("jdbc:mysql:///advjava?useSSL=false", "root", "root");
+         Statement st = con.createStatement();
+         st.executeUpdate("insert into registration values('" + uname + "','" + upass + "','" + ucontact + "')");
+         con.close();
+      } catch (Exception e) {
+         out.println(e);
       }
-
-      var2.sendRedirect("login.html");
-      var3.println("</body></html>");
-      var3.close();
+      response.sendRedirect("login.html");
+      out.println("</body></html>");
+      out.close();
    }
 }
