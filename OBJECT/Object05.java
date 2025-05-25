9038d8896a7753533b56c9128dd44d4a2412b3ed
srohiit2678 <rohitsikhwal99@gmail.com>
2025-05-25 10:07:30 +0530

    Create Object05.java

---
+++ OBJECT/Object05.java
@@ -0,0 +1,23 @@
+
+
+class Object05
+{
+	
+}
+
+
+
+/*
+public final native Class<?> getClass();
+public String toString();
+public native int hashCode();
+public boolean equals(Object);
+pubilc final void wait()throws InterruptedException;
+pubilc native final void wait(long)throws InterruptedException;
+pubilc final void wait(long,int)throws InterruptedException;
+pubilc final void native notify();
+pubilc final native void notifyAll();
+protected void finalize()throws Throwable;
+protected native Object clone()throws CloneNotSupportedException;
+private static native void registerNatives();
+*/
